6346e4d1a612946852f049f99c08faaeb52ad2e8
hayitmurod707 <hayitmurod707@gmail.com>
2022-07-13 16:32:32 +0500

    updated

--- src/Loading/Gradient.jsx
+++ src/Loading/Gradient.jsx
@@ -1,21 +1,26 @@
 import styled, { keyframes } from 'styled-components';
 const spinner = keyframes`
-	to {
+	100% {
 		transform: rotate(1turn);
 	}
 `;
 const StyledElement = styled.div`
 	animation: ${spinner} 0.8s infinite linear;
-	background: conic-gradient(#0000 10%, #0000ff);
 	border-radius: 50%;
 	height: 43px;
-	mask: radial-gradient(farthest-side, #0000, calc(100% - 4px), #000 0);
 	width: 43px;
+	/* mask: radial-gradient(farthest-side, #0000, calc(100% - 4px), #000 0);
+	background: conic-gradient(#0000 10%, #0000ff);
 	-webkit-mask: radial-gradient(
 		farthest-side,
 		#0000,
 		calc(100% - 4px),
 		#000 0
-	);
+	); */
+	background: radial-gradient(farthest-side, #0000ff 94%, #0000) top/5px 5px
+			no-repeat,
+		conic-gradient(#0000 30%, #0000ff);
+	mask: radial-gradient(farthest-side, #0000 calc(100% - 5px), #000 0);
+	-webkit-mask: radial-gradient(farthest-side, #0000 calc(100% - 5px), #000 0);
 `;
 export default StyledElement;
